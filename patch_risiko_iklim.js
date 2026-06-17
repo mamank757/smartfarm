032fb1d35b954dbea88aad8e92f5a11f47726b39
King Mamank <36321851+mamank757@users.noreply.github.com>
2026-06-17 21:30:11 +0800

    Update AMPLIFIKASI_IKLIM value from 1.5 to 2.5

--- patch_risiko_iklim.js
+++ patch_risiko_iklim.js
@@ -136,7 +136,7 @@ function tentukanZonaIklim(lat, lon) {
 // Hasil pengujian skenario El Nino moderat-kuat: mulai dari
 // 1.5 sudah membuat ENSO/IOD moderat mengubah klasifikasi,
 // tanpa membuat "Sangat Kering/Basah Ekstrem" jadi default.
-const AMPLIFIKASI_IKLIM = 1.5;
+const AMPLIFIKASI_IKLIM = 2.5;
 
 function hitungWetnessScore(baselineZOM, ensoVal, iodVal, lat, lon, bulanIndex) {
 
